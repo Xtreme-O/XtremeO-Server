d570f5088e256313e6bde3b809873f553ca056d7
Wahid Ali <1213089@dtu.edu.eg>
2025-12-27 20:02:23 +0200

    Base structure created

---
+++ src/main/java/org/example/xtremo/controller/LoginController.java
@@ -0,0 +1,14 @@
+package org.example.xtremo.controller;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hosam
+ */
+public class LoginController {
+    
+}
---
+++ src/main/java/org/example/xtremo/network/Server.java
@@ -0,0 +1,21 @@
+package org.example.xtremo.network;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+
+
+public class Server {
+    
+    ServerSocket server;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    
+    
+    
+    
+    
+    
+    
+    
+}
