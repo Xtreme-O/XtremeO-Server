cdb598638f188278155fe29dea8c685b5f777f50
Wahid-Qandil <wahidqandill@gmail.com>
2026-01-12 04:02:43 +0200

    Add messaging and move player models to network protocol
    Introduces new models for global messages, in-game messages, invite declined notifications, and move player details. Updates PlayerNetworkOperations to handle new message actions and extends Move model to include player information for enhanced game state communication.

---
+++ src/main/java/org/example/xtremo/network/protocol/models/InGameMessageBody.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.example.xtremo.network.protocol.models;
+
+/**
+ *
+ * @author wahid
+ */
+public class InGameMessageBody {
+    private String message;
+
+    public InGameMessageBody() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public InGameMessageBody(String message) {
+        this.message = message;
+    }
+    
+}
